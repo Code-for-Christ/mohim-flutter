452d1c2dff68300cc45c24e60a75c54dd2046364
kimsungkyum <sungkyum1@naver.com>
2023-04-06 01:15:34 +0900

    feat: 로그아웃 버튼 구현

--- lib/profile/profile_screen.dart
+++ lib/profile/profile_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:get/get.dart';
 import 'package:phonebook/common/component/custom_square_button.dart';
 import 'package:phonebook/common/const/colors.dart';
 import 'package:phonebook/common/const/style.dart';
 import 'package:phonebook/common/layout/default_layout.dart';
+import 'package:phonebook/user/controller/auth_controller.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -13,111 +15,145 @@ class ProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultLayout(
       backgroudColor: Colors.white,
-      child: Column(
-        children: [
-          Container(
-            alignment: Alignment.bottomLeft,
-            height: MediaQuery.of(context).size.height / 4,
-            width: double.infinity,
-            decoration: BoxDecoration(
-                color: PRIMARY_COLOR,
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(24),
-                    bottomRight: Radius.circular(24))),
-            child: Padding(
-              padding: EdgeInsets.only(
-                left: 24,
-                bottom: 24,
-                right: 24,
-              ),
-              child: Wrap(
-                children: [
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      _ProfileImage(),
-                      Gap(40),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            '김성겸B',
-                            style: titleTextStyle.copyWith(
-                                fontWeight: FontWeight.w600, fontSize: 24),
-                          ),
-                          Gap(4),
-                          Text(
-                            '010-7733-7981',
-                            style: body1TextStyle.copyWith(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w500,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.bottomLeft,
+              height: MediaQuery.of(context).size.height / 4,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  color: PRIMARY_COLOR,
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(24),
+                      bottomRight: Radius.circular(24))),
+              child: Padding(
+                padding: EdgeInsets.only(
+                  left: 24,
+                  bottom: 24,
+                  right: 24,
+                ),
+                child: Wrap(
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        _ProfileImage(),
+                        Gap(40),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '김성겸B',
+                              style: titleTextStyle.copyWith(
+                                  fontWeight: FontWeight.w600, fontSize: 24),
                             ),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                ],
+                            Gap(4),
+                            Text(
+                              '010-7733-7981',
+                              style: body1TextStyle.copyWith(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-            child: Center(
-              child: Column(
-                children: [
-                  _InfoBox(
-                    icon: CupertinoIcons.mail_solid,
-                    title: '이메일',
-                    subTitle: 'test@mail.com',
-                  ),
-                  Divider(
-                    color: INPUT_BG_COLOR,
-                    thickness: 1,
-                    height: 30,
-                  ),
-                  _InfoBox(
-                    icon: CupertinoIcons.person_2_fill,
-                    title: '구역',
-                    subTitle: '35구역',
-                  ),
-                  Divider(
-                    color: INPUT_BG_COLOR,
-                    thickness: 1,
-                    height: 30,
-                  ),
-                  _InfoBox(
-                    icon: Icons.auto_awesome,
-                    title: '봉사',
-                    subTitle: '찬양대',
-                  ),
-                  Divider(
-                    color: INPUT_BG_COLOR,
-                    thickness: 1,
-                    height: 30,
-                  ),
-                  _InfoBox(
-                    icon: Icons.bookmark,
-                    title: '소속',
-                    subTitle: '청년회',
-                  ),
-                  Divider(
-                    color: INPUT_BG_COLOR,
-                    thickness: 1,
-                    height: 30,
-                  ),
-                  Gap(60),
-                  CustomSquareButton(
-                      color: PRIMARY_COLOR,
-                      text: '정보수정 요청하기',
-                      filled: true,
-                      onTap: () {},
-                      height: 60)
-                ],
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+              child: Center(
+                child: Column(
+                  children: [
+                    _InfoBox(
+                      icon: CupertinoIcons.mail_solid,
+                      title: '이메일',
+                      subTitle: 'test@mail.com',
+                    ),
+                    Divider(
+                      color: INPUT_BG_COLOR,
+                      thickness: 1,
+                      height: 30,
+                    ),
+                    _InfoBox(
+                      icon: CupertinoIcons.person_2_fill,
+                      title: '구역',
+                      subTitle: '35구역',
+                    ),
+                    Divider(
+                      color: INPUT_BG_COLOR,
+                      thickness: 1,
+                      height: 30,
+                    ),
+                    _InfoBox(
+                      icon: Icons.auto_awesome,
+                      title: '봉사',
+                      subTitle: '찬양대',
+                    ),
+                    Divider(
+                      color: INPUT_BG_COLOR,
+                      thickness: 1,
+                      height: 30,
+                    ),
+                    _InfoBox(
+                      icon: Icons.bookmark,
+                      title: '소속',
+                      subTitle: '청년회',
+                    ),
+                    Divider(
+                      color: INPUT_BG_COLOR,
+                      thickness: 1,
+                      height: 30,
+                    ),
+                    _InfoBox(
+                      icon: Icons.bookmark,
+                      title: '소속',
+                      subTitle: '청년회',
+                    ),
+                    Divider(
+                      color: INPUT_BG_COLOR,
+                      thickness: 1,
+                      height: 30,
+                    ),
+                    _InfoBox(
+                      icon: Icons.bookmark,
+                      title: '소속',
+                      subTitle: '청년회',
+                    ),
+                    Divider(
+                      color: INPUT_BG_COLOR,
+                      thickness: 1,
+                      height: 30,
+                    ),
+                    _InfoBox(
+                      icon: Icons.bookmark,
+                      title: '소속',
+                      subTitle: '청년회',
+                    ),
+                    Divider(
+                      color: INPUT_BG_COLOR,
+                      thickness: 1,
+                      height: 30,
+                    ),
+                    Gap(60),
+                    CustomSquareButton(
+                        color: PRIMARY_COLOR,
+                        text: '로그아웃',
+                        filled: true,
+                        onTap: () {
+                          Get.put(AuthController()).logout();
+                        },
+                        height: 60)
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
@@ -128,12 +164,15 @@ class _ProfileImage extends StatelessWidget {
     Key? key,
   }) : super(key: key);
 
+  final double imageHeight = 180.0;
+  final double imageWidht = 140.0;
+
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(4),
-      height: 120,
-      width: 100,
+      height: 130,
+      width: 110,
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(10),
@@ -148,8 +187,8 @@ class _ProfileImage extends StatelessWidget {
       child: Stack(
         children: [
           Container(
-            width: 70,
-            height: 90,
+            width: imageWidht,
+            height: imageHeight,
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(10), color: INPUT_BG_COLOR),
           ),
@@ -157,8 +196,8 @@ class _ProfileImage extends StatelessWidget {
             borderRadius: BorderRadius.circular(5),
             child: Image.network(
               'https://images.unsplash.com/photo-1574914629385-46448b767aec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bGF0dGV8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
-              width: 140,
-              height: 180,
+              width: imageWidht,
+              height: imageHeight,
               fit: BoxFit.cover,
             ),
           ),
