72dcab7378c0aa9524bc1830b1e756271c865d6b
kimsungkyum <sungkyum1@naver.com>
2023-04-10 17:06:49 +0900

    feat: 봉사별 이름 출력 방식 변경

--- lib/group/component/group_card.dart
+++ lib/group/component/group_card.dart
@@ -32,26 +32,26 @@ class GroupCard extends StatelessWidget {
     return '';
   }
 
-  renderTitle() {
-    if (type == GroupType.cell) {
-      return title;
-    }
-    if (type == GroupType.ministry) {
-      if (title == '청년회') {
-        return '청년회임원';
-      }
-      if (title == '구역') {
-        return '구역임원';
-      }
-      if (title == '교구') {
-        return '교구임원';
-      }
-      return title;
-    }
-    if (type == GroupType.gathering) {
-      return title;
-    }
-  }
+  // renderTitle() {
+  //   if (type == GroupType.cell) {
+  //     return title;
+  //   }
+  //   if (type == GroupType.ministry) {
+  //     if (title == '청년회') {
+  //       return '청년회임원';
+  //     }
+  //     if (title == '구역') {
+  //       return '구역임원';
+  //     }
+  //     if (title == '교구') {
+  //       return '교구임원';
+  //     }
+  //     return title;
+  //   }
+  //   if (type == GroupType.gathering) {
+  //     return title;
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -98,7 +98,7 @@ class GroupCard extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
                   Text(
-                    renderTitle(),
+                    title,
                     style: body1TextStyle.copyWith(fontSize: 22),
                   ),
                   Gap(4),
