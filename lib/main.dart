df6eb2b97a2a977881f7c7ba03f1f1f0cd44e36d
kimsungkyum <sungkyum1@naver.com>
2023-04-17 16:45:33 +0900

    feat: ipad 사용을 위한 세로화면 고정

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:phonebook/common/http/my_http_overrides.dart';
 import 'package:phonebook/common/const/colors.dart';
 import 'package:phonebook/common/view/splash_screen.dart';
 
-void main() {
+void main() async {
   HttpOverrides.global = MyHttpOverrides();
-
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(_App());
 }
 
